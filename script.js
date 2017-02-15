4726d090649990bc5700b11dd8491e78b6cff67c
Jesse Hernandez <jesse4leg@gmail.com>
2017-02-15 21:06:20 +0000

    Drew face

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //Write your function calls below this line
+ setColor (0,0,"black");
+ setColor (0,9,"black");
+ setColor (9,0,"black");
+ setColor (9,9,"black");
+ setColor (2,3,"yellow");
+ setColor(2,6,"yellow");
+ setColor(5,1,"blue");
+ setColor(6,2,"blue");
+ setColor(6,3,"blue");
+ setColor(6,4,"blue");
+  setColor(6,5,"blue");
+   setColor(6,6,"blue");
+  setColor(6,7,"blue");
+  setColor(5,8,"blue");
